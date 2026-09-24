d370b675f05825ae2a5807e8c1308f77856c2eb6
HAMMOUMI.W <hammoumi.w@U-3ZPKK3U5TW2Q.workspaces.services.aws.creos.internal>
2026-09-24 16:25:41 +0200

    Add CI quality gate

--- app/admin-books/webapp/test/wdi5/specs/bookshop.test.js
+++ app/admin-books/webapp/test/wdi5/specs/bookshop.test.js
@@ -1,6 +1,6 @@
 const ManageBooksPage = require("../pageobjects/manageBooks.page");
 const NewBookPage = require("../pageobjects/newBook.page");
 
-describe("Manage bookshop", () => {
-
-});
+//describe("Manage bookshop", () => {
+//
+//});
